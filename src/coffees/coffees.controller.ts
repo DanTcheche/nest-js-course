858d6c949bef31a210f72f8a6a459a5c90bb5834
Dan Tcheche <dtcheche@gmail.com>
2022-03-02 16:00:01 -0300

    Add flavor entity and relation

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,17 +1,28 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
-import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll(@Res() response, @Query() paginationQuery) {
+  findAll() {
     //const { limit, offset } = paginationQuery;
-    const response_message = this.coffeesService.findAll();
-    return response.status(200).send(response_message);
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
